5152e50bb29de91af9bd1e92314b6874da56a380
sehaim <seha3722@gmail.com>
2026-02-13 11:07:52 +0900

    :sparkles: feat: 티켓 조회 api 연동 완료

--- src/pages/ticket/TicketListPage.tsx
+++ src/pages/ticket/TicketListPage.tsx
@@ -1,30 +1,36 @@
 import clsx from "clsx";
+import { useEffect, useState } from "react";
 import type { UserTicket } from "@/types/ticket";
 import TicketList from "@/components/ticket/TicketList";
 import Button from "@/components/common/Button";
 import { Link } from "react-router-dom";
 import { IoTicket } from "react-icons/io5";
-
-const tickets: UserTicket[] = [
-  {
-    userTicketId: 10249381,
-    availableAt: "2026-02-12T09:00:00",
-    activeStatus: "ACTIVE",
-    ticketType: "PREMIUM",
-    paymentDate: "2026-02-11T14:25:00",
-  },
-  {
-    userTicketId: 10249382,
-    availableAt: "2026-02-14T09:00:00",
-    activeStatus: "DEACTIVE",
-    ticketType: "GENERAL",
-    paymentDate: "2026-02-10T18:40:00",
-  },
-];
+import LoadingSpinner from "@/components/common/LoadingSpinner";
+import { getMyTicketList } from "@/api/ticket.api";
 
 export default function TicketListPage() {
+  const [tickets, setTickets] = useState<UserTicket[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchMyTickets = async () => {
+      try {
+        setIsLoading(true);
+        const data = await getMyTicketList();
+        setTickets(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    void fetchMyTickets();
+  }, []);
+
   return (
     <div className={clsx("container")}>
+      <LoadingSpinner isLoading={isLoading} />
       <div className={clsx('page-title')}>
         <div className={clsx('glass', 'title-icon-container')}>
           <IoTicket className={clsx('title-icon')} />
